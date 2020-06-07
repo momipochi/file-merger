04798f38e330f3f052acc16615fa714189c785a6
momipochi <40135161+momipochi@users.noreply.github.com>
2020-06-07 21:51:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,24 +13,60 @@ int main() {
     fileA.open("Book1.csv");
     fileB.open("Book2.csv");
 
-
-    if(fileA.is_open() || fileB.is_open()){
+if(fileA.is_open() || fileB.is_open()){
         string lineA,lineB;
         bool key1 = true, key2=true;
         while(key1 || key2){
-            string row ="";
-            if(getline(fileA,lineA,'\n')){
-                row += lineA+",";
+            string lineBuild ="";
+            vector<string> tmpA;
+            vector<string> tmpB;
+            if(getline(fileA,lineA)){
+
+                stringstream a(lineA);
+                string wordA;
+                while(getline(a,wordA,',')){
+                    tmpA.push_back(wordA);
+                }
             }else{
-                key1=false;
+                key1 = false;
             }
-            if(getline(fileB,lineB,'\n')){
-                row += lineB;
+            if(getline(fileB,lineB)){
+                stringstream b(lineB);
+                string wordB;
+                while(getline(b,wordB,',')){
+                    tmpB.push_back(wordB);
+                }
+
             }else{
-                key2=false;
+                key2 = false;
             }
-            v.push_back(row);
-        };
+
+            if(tmpA.size() == 0){
+                for(int i =0;i<tmpB.size();i++){
+                    lineBuild+=",";
+                }
+                for(string str:tmpB){
+                    lineBuild += str+",";
+                }
+            }else if(tmpB.size() == 0){
+                for(int i =0;i<tmpA.size();i++){
+                    lineBuild+=",";
+                }
+                for(string str:tmpA){
+                    lineBuild += str+",";
+                }
+            }else {
+                for(string str:tmpA){
+                    lineBuild += str+",";
+                }
+                for(int i =0;i<tmpB.size()-1;i++){
+                    lineBuild += tmpB[i]+",";
+                }
+                lineBuild += tmpB[tmpB.size()-1];
+            }
+
+            v.push_back(lineBuild);
+        }
     }
 
 
